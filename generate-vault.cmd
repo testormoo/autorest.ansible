df285a1a167cc8266cbe5c986edb009100e5f3f9
zikalino <zikalino@microsoft.com>
2018-02-02 14:32:19 +0800

    regenerating keyvault

--- generate-vault.cmd
+++ generate-vault.cmd
@@ -1,15 +1,14 @@
-rem test
-rmdir /S /Q c:\dev\ansible-hatchery\role\modules\azure_rm_keyvault*
-rmdir /S /Q c:\dev\ansible-hatchery\all\modules\azure_rm_keyvault*
-rmdir /S /Q c:\dev\ansible-hatchery\role\tests\azure_rm_keyvault*
-rmdir /S /Q c:\dev\ansible-hatchery\all\tests\azure_rm_keyvault*
-rmdir /S /Q c:\dev\ansible-hatchery\template\azure_rm_keyvault*
+
+del c:\dev\ansible-hatchery\library\azure_rm_keyvault.py
+del c:\dev\ansible-hatchery\library\azure_rm_keyvault_facts.py
+rmdir /S /Q c:\dev\ansible-hatchery\tests\integration\targets\azure_rm_keyvault
+rmdir /S /Q c:\dev\ansible-hatchery\tests\integration\targets\azure_rm_keyvault_facts
+rmdir /S /Q c:\dev\tmp
 
 cd c:\dev\azure-rest-api-specs\specification\keyvault\resource-manager
-call autorest --output-folder=c:\dev\ansible-hatchery\ --use=c:\dev\autorest.ansible\ --python --tag=package-2016-10
+call autorest --output-folder=c:\dev\tmp\ --use=c:\dev\autorest.ansible\ --python --tag=package-2016-10
 
-xcopy /S c:\dev\ansible-hatchery\python\prs\* c:\dev\ansible-hatchery\prs\
-xcopy /S c:\dev\ansible-hatchery\python\role\* c:\dev\ansible-hatchery\role\
-xcopy /S c:\dev\ansible-hatchery\python\all\* c:\dev\ansible-hatchery\all\
-xcopy /S c:\dev\ansible-hatchery\python\template\* c:\dev\ansible-hatchery\template\
-rmdir /S /Q c:\dev\ansible-hatchery\python
+xcopy /S c:\dev\tmp\python\all\modules\* c:\dev\ansible-hatchery\library\
+xcopy /S c:\dev\tmp\python\all\tests\* c:\dev\ansible-hatchery\tests\integration\targets\
+xcopy /S c:\dev\tmp\all\modules\* c:\dev\ansible-hatchery\library\
+xcopy /S c:\dev\tmp\all\tests\* c:\dev\ansible-hatchery\tests\integration\targets\
