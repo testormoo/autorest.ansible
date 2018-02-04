33df379316ead7d36ed10ace26d91a8d33cd5e41
zikalino <zikalino@microsoft.com>
2018-02-04 16:36:03 +0800

    fixed entry point

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -8,7 +8,7 @@ cd /autorest.ansible/scripts
 chmod 777 generate*
 ./generate-$1.sh
 
-cp /ansible-hatchery-tmp/python/all/modules/* /ansible-hatchery/library/
-cp /ansible-hatchery-tmp/python/all/tests/* /ansible-hatchery/tests/integration/targets
-cp /ansible-hatchery-tmp/all/modules/* /ansible-hatchery/library/
-cp /ansible-hatchery-tmp/all/tests/* /ansible-hatchery/tests/integration/targets
+cp -R /ansible-hatchery-tmp/python/all/modules/* /ansible-hatchery/library/
+cp -R /ansible-hatchery-tmp/python/all/tests/* /ansible-hatchery/tests/integration/targets
+cp -R /ansible-hatchery-tmp/all/modules/* /ansible-hatchery/library/
+cp -R /ansible-hatchery-tmp/all/tests/* /ansible-hatchery/tests/integration/targets
