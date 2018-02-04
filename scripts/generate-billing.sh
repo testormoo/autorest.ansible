96bcf0da577846b0887da694e5b73c03de27a1e9
zikalino <zikalino@microsoft.com>
2018-02-04 14:31:37 +0800

    updated tmp folder

--- scripts/generate-billing.sh
+++ scripts/generate-billing.sh
@@ -1,2 +1,2 @@
 cd /azure-rest-api-specs/specification/billing/resource-manager
-autorest --output-folder=/ansible-hatchery/tmp --use=/autorest.ansible --python --tag=package-2017-04-preview
+autorest --output-folder=/ansible-hatchery-tmp --use=/autorest.ansible --python --tag=package-2017-04-preview
