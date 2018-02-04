a7bc5107b32a0b3e77a9be4be13acd3455694e9c
zikalino <zikalino@microsoft.com>
2018-02-04 14:49:21 +0800

    try to run mysql twice

--- dockerfile/run.bat
+++ dockerfile/run.bat
@@ -1,3 +1,3 @@
 docker kill autorest-ansible
 docker rm autorest-ansible
-docker run -t -i --name autorest-ansible -v c:/dev/ansible-hatchery:/ansible-hatchery -v c:/dev/tmp:/ansible-hatchery-tmp dockiot/autorest-ansible billing xxx yyy zzz
+docker run -t -i --name autorest-ansible -v c:/dev/ansible-hatchery:/ansible-hatchery -v c:/dev/tmp:/ansible-hatchery-tmp dockiot/autorest-ansible %1
--- scripts/generate-mysql.sh
+++ scripts/generate-mysql.sh
@@ -1,2 +1,3 @@
 cd /azure-rest-api-specs/specification/mysql/resource-manager
 autorest --output-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2017-04-preview
+autorest --output-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2017-04-preview
