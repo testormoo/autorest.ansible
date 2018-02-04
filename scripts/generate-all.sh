e82ac59f0435bea19f03504d6015da3b1b4808ff
zikalino <zikalino@microsoft.com>
2018-02-04 15:54:34 +0800

    fixed generate-all script

--- scripts/generate-all.sh
+++ scripts/generate-all.sh
@@ -4,14 +4,14 @@ rm -rf /ansible-hatchery/library/*
 rm -rf /ansible-hatchery/tests/integration/targets/*
 
 # generate all the modules API by API
-/ansible-hatchery/generate-sql.sh
-/ansible-hatchery/generate-mysql.sh
-/ansible-hatchery/generate-postgresql.sh
-/ansible-hatchery/generate-authorization.sh
-/ansible-hatchery/generate-web.sh
-/ansible-hatchery/generate-network.sh
-/ansible-hatchery/generate-containerinstance.sh
-/ansible-hatchery/generate-containerregistry.sh
-/ansible-hatchery/generate-keyvault.sh
-/ansible-hatchery/generate-batch.sh
-/ansible-hatchery/generate-batchai.sh
+/autorest.ansible/scripts/generate-sql.sh
+/autorest.ansible/scripts/generate-mysql.sh
+/autorest.ansible/scripts/generate-postgresql.sh
+/autorest.ansible/scripts/generate-authorization.sh
+/autorest.ansible/scripts/generate-web.sh
+/autorest.ansible/scripts/generate-network.sh
+/autorest.ansible/scripts/generate-containerinstance.sh
+/autorest.ansible/scripts/generate-containerregistry.sh
+/autorest.ansible/scripts/generate-keyvault.sh
+/autorest.ansible/scripts/generate-batch.sh
+/autorest.ansible/scripts/generate-batchai.sh
