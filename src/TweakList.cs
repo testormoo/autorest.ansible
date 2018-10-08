064b45b32ca59624eafd77f4b9d9014e6613bcaa
zikalino <zikalino@microsoft.com>
2018-10-08 09:26:21 +0800

    adding tags

--- src/TweakList.cs
+++ src/TweakList.cs
@@ -17,6 +17,7 @@ namespace AutoRest.Ansible
             new Tweak_Response_AddField("*", "version"),
             new Tweak_Response_AddField("*", "state"),
             new Tweak_Response_AddField("*", "user_visible_state"),
+            new Tweak_Response_AddField("*", "tags"),
             new Tweak_Option_Documentation("*", "location", "Resource location. If not set, location from the resource group will be used as default."),
             //new Tweak_Option_DefaultValueTest("*", "location", "eastus"),
             new Tweak_Option_DefaultValueSample("*", "location", "eastus"),
