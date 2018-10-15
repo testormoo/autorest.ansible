cafa372aadaab5591e1ca374bdf6eb1338e0e90d
zikalino <zikalino@microsoft.com>
2018-10-15 17:40:53 +0800

    dump more info when generation mods

--- scripts/generate-all.sh
+++ scripts/generate-all.sh
@@ -10,12 +10,21 @@ echo "Generating MySQL"
 /autorest.ansible/scripts/generate-mysql.sh
 echo "Generating PostgreSQL"
 /autorest.ansible/scripts/generate-postgresql.sh
+echo "Generating Authorization"
 /autorest.ansible/scripts/generate-authorization.sh
+echo "Generating Web"
 /autorest.ansible/scripts/generate-web.sh
+echo "Generating Network"
 /autorest.ansible/scripts/generate-network.sh
+echo "Generating Container Instance"
 /autorest.ansible/scripts/generate-containerinstance.sh
+echo "Generating Container Registry"
 /autorest.ansible/scripts/generate-containerregistry.sh
+echo "Generating KeyVault"
 /autorest.ansible/scripts/generate-keyvault.sh
+echo "Generating Batch"
 /autorest.ansible/scripts/generate-batch.sh
+echo "Generating Batch AI"
 /autorest.ansible/scripts/generate-batchai.sh
+echo "Generating Cosmos"
 /autorest.ansible/scripts/generate-cosmos.sh
