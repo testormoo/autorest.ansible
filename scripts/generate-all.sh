a96824212bf576e5efeab454d884efd8422c3761
zikalino <zikalino@microsoft.com>
2018-10-16 14:26:54 +0800

    make output more readable

--- scripts/generate-all.sh
+++ scripts/generate-all.sh
@@ -4,27 +4,27 @@ rm -rf /ansible-hatchery/library/*
 rm -rf /ansible-hatchery/tests/integration/targets/*
 
 # generate all the modules API by API
-echo "Generating SQL"
+echo "----------------------------- Generating SQL"
 /autorest.ansible/scripts/generate-sql.sh
-echo "Generating MySQL"
+echo "----------------------------- Generating MySQL"
 /autorest.ansible/scripts/generate-mysql.sh
-echo "Generating PostgreSQL"
+echo "----------------------------- Generating PostgreSQL"
 /autorest.ansible/scripts/generate-postgresql.sh
-echo "Generating Authorization"
+echo "----------------------------- Generating Authorization"
 /autorest.ansible/scripts/generate-authorization.sh
-echo "Generating Web"
+echo "----------------------------- Generating Web"
 /autorest.ansible/scripts/generate-web.sh
-echo "Generating Network"
+echo "----------------------------- Generating Network"
 /autorest.ansible/scripts/generate-network.sh
-echo "Generating Container Instance"
+echo "----------------------------- Generating Container Instance"
 /autorest.ansible/scripts/generate-containerinstance.sh
-echo "Generating Container Registry"
+echo "----------------------------- Generating Container Registry"
 /autorest.ansible/scripts/generate-containerregistry.sh
-echo "Generating KeyVault"
+echo "----------------------------- Generating KeyVault"
 /autorest.ansible/scripts/generate-keyvault.sh
-echo "Generating Batch"
+echo "----------------------------- Generating Batch"
 /autorest.ansible/scripts/generate-batch.sh
-echo "Generating Batch AI"
+echo "----------------------------- Generating Batch AI"
 /autorest.ansible/scripts/generate-batchai.sh
-echo "Generating Cosmos"
+echo "----------------------------- Generating Cosmos"
 /autorest.ansible/scripts/generate-cosmos.sh
