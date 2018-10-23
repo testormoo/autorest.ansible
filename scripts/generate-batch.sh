36226dba27a8fc5491ee6a6ab9e18cfe6048d8bc
zikalino <zikalino@microsoft.com>
2018-10-23 17:14:36 +0800

    don't copy batch thing

--- scripts/generate-batch.sh
+++ scripts/generate-batch.sh
@@ -1,3 +1,3 @@
 cd /azure-rest-api-specs/specification/batch/resource-manager
 autorest --output-folder=/ansible-hatchery-tmp/ --python-sdks-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2017-09
-cp -r  /ansible-hatchery-tmp/azure-mgmt-batch/* /ansible-hatchery-tmp && rm -R /ansible-hatchery-tmp/azure-mgmt-batch/*
+#cp -r  /ansible-hatchery-tmp/azure-mgmt-batch/* /ansible-hatchery-tmp && rm -R /ansible-hatchery-tmp/azure-mgmt-batch/*
