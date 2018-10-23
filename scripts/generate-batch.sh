8f7e393f41be268b7a33e6f3f935f8ffb05877f2
zikalino <zikalino@microsoft.com>
2018-10-23 13:51:42 +0800

    mv instead of cp

--- scripts/generate-batch.sh
+++ scripts/generate-batch.sh
@@ -1,3 +1,3 @@
 cd /azure-rest-api-specs/specification/batch/resource-manager
 autorest --output-folder=/ansible-hatchery-tmp/ --python-sdks-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2017-09
-cp /ansible-hatchery-tmp/azure-mgmt-batch/* /ansible-hatchery-tmp
+mv /ansible-hatchery-tmp/azure-mgmt-batch/* /ansible-hatchery-tmp
--- scripts/generate-batchai.sh
+++ scripts/generate-batchai.sh
@@ -1,3 +1,3 @@
 cd /azure-rest-api-specs/specification/batchai/resource-manager
 autorest --output-folder=/ansible-hatchery-tmp/ --python-sdks-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2017-09-preview
-cp /ansible-hatchery-tmp/azure-mgmt-batchai/* /ansible-hatchery-tmp
+mv /ansible-hatchery-tmp/azure-mgmt-batchai/* /ansible-hatchery-tmp
