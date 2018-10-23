2b0a8bd8f2659da34744bd65c5c4d1f30cbeea12
zikalino <zikalino@microsoft.com>
2018-10-23 16:07:03 +0800

    another settings check

--- src/Program.cs
+++ src/Program.cs
@@ -63,10 +63,11 @@ namespace AutoRest.Ansible
 
             // build settings
             var altNamespace = (await GetValue<string[]>("input-file") ?? new[] { "" }).FirstOrDefault()?.Split('/').Last().Split('\\').Last().Split('.').First();
-            
+            var namespace = await GetValue("namespace");
+            namespace = "xx" + namespace + "xx";
             new Settings
             {
-                Namespace = await GetValue("namespace"),
+                Namespace = namespace,
                 ClientName = GetXmsCodeGenSetting<string>(codeModelT, "name") ?? await GetValue("override-client-name"),
                 PayloadFlatteningThreshold = GetXmsCodeGenSetting<int?>(codeModelT, "ft") ?? await GetValue<int?>("payload-flattening-threshold") ?? 0,
                 AddCredentials = await GetValue<bool?>("add-credentials") ?? false,
