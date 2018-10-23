476b123d4a1e06f5c713c6c6bc98141cfee69baa
zikalino <zikalino@microsoft.com>
2018-10-23 17:35:46 +0800

    just a few more tests

--- scripts/generate-batch.sh
+++ scripts/generate-batch.sh
@@ -1,3 +1,5 @@
 cd /azure-rest-api-specs/specification/batch/resource-manager
+echo BATCH GENERATING
 autorest --output-folder=/ansible-hatchery-tmp/ --python-sdks-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2017-09
-#cp -r  /ansible-hatchery-tmp/azure-mgmt-batch/* /ansible-hatchery-tmp && rm -R /ansible-hatchery-tmp/azure-mgmt-batch/*
+echo BATCH GENERATED
+cp -r  /ansible-hatchery-tmp/azure-mgmt-batch/* /ansible-hatchery-tmp
