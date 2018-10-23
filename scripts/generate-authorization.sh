55d1b2ba25cc4d64eff6128c5306b41da6e970fe
zikalino <zikalino@microsoft.com>
2018-10-23 21:15:15 +0800

    fix authorization

--- scripts/generate-authorization.sh
+++ scripts/generate-authorization.sh
@@ -1,2 +1,3 @@
 cd /azure-rest-api-specs/specification/authorization/resource-manager
 autorest --output-folder=/ansible-hatchery-tmp/ --python-sdks-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2015-07
+cp -r /ansible-hatchery-tmp/* /ansible-hatchery-tmpx/
