761d92357a4824aad41c06760f993e23f4f3b401
zikalino <zikalino@microsoft.com>
2018-10-23 12:50:13 +0800

    add python-sdks-folder everywhere

--- scripts/generate-keyvault.sh
+++ scripts/generate-keyvault.sh
@@ -1,2 +1,2 @@
 cd /azure-rest-api-specs/specification/keyvault/resource-manager
-autorest --output-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2016-10
+autorest --output-folder=/ansible-hatchery-tmp/ --output-folder=/python-sdks-folder/ --use=/autorest.ansible --python --tag=package-2016-10
--- scripts/generate-network.sh
+++ scripts/generate-network.sh
@@ -1,2 +1,2 @@
 cd /azure-rest-api-specs/specification/network/resource-manager
-autorest --output-folder=/ansible-hatchery-tmp/ --use=/autorest.ansible --python --tag=package-2017-10
+autorest --output-folder=/ansible-hatchery-tmp/ --output-folder=/python-sdks-folder/ --use=/autorest.ansible --python --tag=package-2017-10
